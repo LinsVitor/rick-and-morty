26d2858ba29773662e5b43b3ea3fa5b966d12c67
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2025-10-20 11:47:19 -0300

    Migra a sua função para o arquivo utils.js

--- script/utils/arrayofnumbers.js
+++
@@ -1,9 +0,0 @@
-export function arrayOfNumbers(start, end, step = 1) {
-	let array = [];
-	let number = start;
-	for (let i = start; i <= end; i++) {
-		number += step;
-		array.push(number);
-	}
-	return array;
-}
