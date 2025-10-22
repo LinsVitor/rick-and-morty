52255241478f015c8dea968aa8b8dae6a86dae3b
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2025-10-22 09:52:52 -0300

    Pequenas mudanças sem afetar a lógica principal das funções

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 "use strict";
 import { Character, Episode, Location } from "./utils/rickdex.js";
-import { translate, arrayOfNumbers, truncate } from "./utils/utils.js";
+import { arrayOfNumbers, translate, truncate } from "./utils/utils.js";
 
 // Instancia as classes responsável por manipular a API
 const character = new Character();
@@ -12,13 +12,14 @@ async function updateScreen(type, ids) {
 	const mainGrid = document.getElementById("mainGrid");
 	const mainTitle = document.getElementById("mainTitle");
 	const elements = await type.getAll(ids);
-	
+
 	switch (type) {
 		case character:
 			mainTitle.innerText = "Personagens";
 			mainGrid.classList.add("main__character-grid");
 			mainGrid.innerHTML = "";
 			for (const character of elements) {
+				console.log(character);
 				const characterCard = `
     		<article class="main__card">
 				<img
@@ -27,9 +28,9 @@ async function updateScreen(type, ids) {
 						/>
 				<h3 class="main__character-name">${truncate(character.name)}</h3>
 				<ul class="main__character-info">
-					<li><i class="bx-pulse"></i>${translate(character.status)}</li>
-					<li><i class="bx-alien"></i>${translate(character.species)}</li>
-					<li><i class="bx-planet"></i>${translate(character.origin.name)}</li>
+					<li><i class="bx-pulse"></i>${translate("status", character.status)}</li>
+					<li><i class="bx-alien"></i>${translate("species", character.species)}</li>
+					<li><i class="bx-planet"></i>${translate("origin", character.origin.name)}</li>
 				</ul>
 				<div class="main__character-buttons">
 				<button class="main__character-info-btn">
@@ -51,10 +52,10 @@ async function updateScreen(type, ids) {
 }
 
 // Função com a lógica de paginação
-async function navigation(type, ids) {
+async function navigation(type, ids, step) {
 	let startArray = ids;
 	let page = 1;
-	const maxPage = await type.info().then((dados) => dados["count"] / 8);
+	const maxPage = await type.info().then((dados) => dados["count"] / step);
 	const nextPage = document.getElementById("nextPage");
 	const prevPage = document.getElementById("prevPage");
 	nextPage.addEventListener("click", nextCharacter);
@@ -62,18 +63,24 @@ async function navigation(type, ids) {
 
 	function nextCharacter() {
 		if (page >= 1 && page <= maxPage) {
-			updateScreen(type, arrayOfNumbers(startArray[0] + 8, startArray[1] + 8));
-			startArray[0] += 8;
-			startArray[1] += 8;
+			updateScreen(
+				type,
+				arrayOfNumbers(startArray[0] + step, startArray[1] + step)
+			);
+			startArray[0] += step;
+			startArray[1] += step;
 			page++;
 		}
 	}
 
 	function previousCharacter() {
 		if (page > 1) {
-			updateScreen(type, arrayOfNumbers(startArray[0] - 8, startArray[1] - 8));
-			startArray[0] -= 8;
-			startArray[1] -= 8;
+			updateScreen(
+				type,
+				arrayOfNumbers(startArray[0] - step, startArray[1] - step)
+			);
+			startArray[0] -= step;
+			startArray[1] -= step;
 			page--;
 		}
 	}
@@ -83,4 +90,4 @@ window.onload = function () {
 	updateScreen(character, arrayOfNumbers(0, 7));
 };
 
-navigation(character, [0, 7]);
+navigation(character, [0, 7], 8);
